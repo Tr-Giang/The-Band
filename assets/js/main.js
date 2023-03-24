d5f3d3b664ee8c1a68f1ed11b60e86b4ae458e2b
Nguyễn Trường Giang <nguyentruonggiang190309@gmail.com>
2023-03-24 16:31:26 +0700

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,15 +16,50 @@ function sliderAnimation() {
 }
 
 function discolorationText() {
+    const authorNode = document.querySelector('.author');
     var angles = 0;
 
     setInterval(() => {
-        link.style.filter = `hue-rotate(${angles}deg)`;
+        authorNode.style.filter = `hue-rotate(${angles}deg)`;
         angles += 40;
         if (angles > 360) angles = 0;
     }, 500);
 
 }
 
+function handleBtn() {
+    const btnElements = document.querySelectorAll('[data-menu]');
+    const fatherElements = document.querySelectorAll('[data-father]');
+    const linkItems = document.querySelectorAll('.nav-item>a');
+    btnElements.forEach((btnElement, index) => {
+        let menuID = btnElement.dataset.menu;
+        const menu = document.querySelector(`[data-father="${menuID}"]`);
+
+        btnElement.onclick = () => {
+            if (!menu.classList.contains('active')) {
+                menu.classList.add('active');
+            }
+            else {
+                if (menu.querySelector('.active')) {
+                    menu.classList.remove('active');
+                    menu.querySelector('.active').classList.remove('active');
+                } else {
+                    menu.classList.remove('active');
+
+                }
+            }
+        };
+    });
+    linkItems.forEach((linkItem) => {
+        linkItem.onclick = () => {
+            fatherElements.forEach((item) => {
+                item.classList.remove('active');
+            });
+        };
+    });
+
+}
+
 sliderAnimation();
 discolorationText();
+handleBtn();
